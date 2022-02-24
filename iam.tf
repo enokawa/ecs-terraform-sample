5c48c4c9a9798ad8b74bb949dc55926ac56fe236
enokawa <enokawa00@gmail.com>
2022-02-24 12:03:48 +0900

    Add IAM

---
+++ iam.tf
@@ -0,0 +1,38 @@
+data "aws_iam_policy_document" "ecs_task_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "${var.stage}-ecs-task-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${var.stage}-ecs-task-execution-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonECSTaskExecutionRolePolicy_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
